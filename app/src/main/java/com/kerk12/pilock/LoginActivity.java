4aed5baad3456c5af0d772cf52c4dade82f86780
Kyriakos Giannakis <kerk12gr@gmail.com>
2017-04-02 02:46:57 +0300

    Initial Commit.

---
+++ app/src/main/java/com/kerk12/pilock/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.kerk12.pilock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
