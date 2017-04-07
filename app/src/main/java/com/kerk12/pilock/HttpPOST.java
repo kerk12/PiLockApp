7a9ba2c4a6ba79a282d032c96e8ad8f3830d1f3d
Kyriakos Giannakis <kerk12gr@gmail.com>
2017-04-08 00:14:28 +0300

    Started implementing the HttpPOST Class.

--- app/src/main/java/com/kerk12/pilock/HttpPOST.java
+++ app/src/main/java/com/kerk12/pilock/HttpPOST.java
@@ -1,7 +1,22 @@
 package com.kerk12.pilock;
 
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ResponseCache;
 import java.net.URL;
+import java.security.GeneralSecurityException;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
 
 /**
  * Created by kgiannakis on 4/4/2017.
@@ -11,11 +26,134 @@ public class HttpPOST {
     private URL url = null;
     private Map<String, String> params = null;
     private String result = null;
+    private boolean Executed = false;
+    private int ResponseCode = 200;
+    private String ErrorStream = null;
+    private boolean HasErrors = false;
+    private boolean CertError = false;
 
-    public HttpPOST(URL url, Map<String, String> params, String result) {
+    public HttpPOST(URL url, Map<String, String> params) {
         this.url = url;
         this.params = params;
-        this.result = result;
     }
-    
+
+    private class POSTTask extends AsyncTask<Void, Void, String>{
+
+        /**
+         * Sends the POST request and recieves a result string.
+         * @return The result String if no errors occured, null if an error occured.
+         * @throws IOException When a connection fails, as well as when the certificate cannot be read.
+         * @throws GeneralSecurityException If a secure connection cannot be established.
+         */
+        private String getResult() throws IOException, GeneralSecurityException {
+
+            // Set the SSLContext, so that it trusts our Self-Signed Certificate.
+            SSLContext sslContext = CustomSSLTruster.TrustCertificate();
+            HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+            conn.setSSLSocketFactory(sslContext.getSocketFactory());
+
+            // Set the connection parameters...
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setConnectTimeout(10000);
+            conn.setReadTimeout(15000);
+
+            // Write the POST data...
+            QueryBuilder builder = new QueryBuilder(params);
+            OutputStream os = conn.getOutputStream();
+
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+            writer.write(builder.getQuery());
+            writer.flush();
+            writer.close();
+
+            ResponseCode = conn.getResponseCode();
+
+            // Get the input if there was no error, else, get the ErrorStream and return null.
+            if (ResponseCode == HttpsURLConnection.HTTP_OK){
+                // TODO Make it more beautiful.
+                InputStream is = conn.getInputStream();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"), 8);
+                String line = null;
+                StringBuilder sb = new StringBuilder();
+
+                while ((line = reader.readLine()) != null){
+                    sb.append(line + "\n");
+                }
+                reader.close();
+
+                return sb.toString();
+            } else {
+                InputStream ErrorIStream = conn.getErrorStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(ErrorIStream, "UTF-8"), 8);
+                String line = null;
+                StringBuilder sb = new StringBuilder();
+
+                while ((line = reader.readLine()) != null){
+                    sb.append(line + "\n");
+                }
+                reader.close();
+
+                ErrorStream = sb.toString();
+                HasErrors = true;
+            }
+            return null;
+
+        }
+
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                return getResult();
+            } catch (IOException e) {
+                e.printStackTrace();
+                HasErrors = true;
+                CertError = true;
+            } catch (GeneralSecurityException e) {
+                e.printStackTrace();
+                HasErrors = true;
+                CertError = true;
+            }
+            return null;
+        }
+    }
+
+    public String getResult(){
+        if (!Executed){
+            POSTTask t = new POSTTask();
+            try {
+                result = t.execute().get();
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                HasErrors = true;
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+                HasErrors = true;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Returns the response code recieved from the server after executing the request.
+     * @return The response code.
+     */
+    public int getResponseCode() {
+        return ResponseCode;
+    }
+
+    public String getErrorStream() {
+        return ErrorStream;
+    }
+
+    public boolean HasErrors() {
+        return HasErrors;
+    }
+
+    public boolean HasCertError() {
+        return CertError;
+    }
 }
--- app/src/main/java/com/kerk12/pilock/PINEntryActivity.java
+++ app/src/main/java/com/kerk12/pilock/PINEntryActivity.java
@@ -1,14 +1,51 @@
 package com.kerk12.pilock;
 
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.GeneralSecurityException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+
 public class PINEntryActivity extends AppCompatActivity {
+
+    private String AuthToken = null;
+    private String ServerURL = null;
+    private String PIN = null;
+
+
 
     public static final String AUTH_TOKEN_KEY = "authToken";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pinentry);
+
+        /*
+         * Initialize the auth token and server url.
+         */
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences authPrefs = getSharedPreferences(getResources().getString(R.string.auth_prefs), MODE_PRIVATE);
+        ServerURL = sharedPrefs.getString(SettingsActivity.SERVER_ADDRESS_KEY, "none");
+        AuthToken = authPrefs.getString(AUTH_TOKEN_KEY, "None");
+
+
+
     }
 }
